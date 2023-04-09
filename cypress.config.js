27cf92fb5f9e35f0030195d87a156d4e479c0f34
Anshita Bhasin <anshita.bhasin@gmail.com>
2023-04-09 18:21:50 +0400

    cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on) {
-      /* block iamge load
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        console.log(launchOptions.args)
-        if (browser.family === 'chromium' && browser.name !== 'electron') {
-          launchOptions.args.push('--blink-settings=imagesEnabled=false')
-          return launchOptions
-        }
+      cypressSplit(on, config)
+      return config
 
-      })
-      */
     },
   },
- 
+
 
 });
